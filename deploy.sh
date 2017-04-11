a39d9584f1734ef2e46bde4db46eb00db2386bd4
tcdw <admin@tcdw.net>
2017-04-11 15:57:11 +0800

    fix sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 git pull origin master
-build -l data.json -t links.ejs -o ../document/links.md
-python3 /opt/SmartBlog/manage.py build-rss
-bash /opt/SmartBlog/update-gh.sh
+build -l data.json -t links.ejs -o ../SmartBlog/document/links.md
